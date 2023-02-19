11fc33c8276d4d987068aaf5a95292a3fb44dad9
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-02-19 10:24:03 +0300

    Handle NotFoundException

--- src/main/java/com/example/demo/exception/NotFoundException.java
+++ src/main/java/com/example/demo/exception/NotFoundException.java
@@ -1,9 +1,5 @@
 package com.example.demo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 // We have to types of exceptions:
 // run time and check exception(with try and catch)
 public class NotFoundException extends RuntimeException {
