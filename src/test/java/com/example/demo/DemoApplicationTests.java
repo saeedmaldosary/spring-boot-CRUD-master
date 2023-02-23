0cc46a1bb1d013c3f7d8329d9ba3af9889f21825
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-02-23 13:28:35 +0300

    Writing Unit Tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// If we have an integration test we should use below annotation
 @SpringBootTest
 class DemoApplicationTests {
 
